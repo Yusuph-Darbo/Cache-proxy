46c81808a2d3975fac1ba5efefd0d8220d1b5fcb
Yusuph Darbo <yusuphdarbo@gmail.com>
2026-07-12 15:27:11 +0100

    function to connect to redis

--- src/cache/cache-store.ts
+++ src/cache/cache-store.ts
@@ -4,4 +4,10 @@ const client = createClient();
 
 client.on("error", (err) => console.log("Redis Client Error", err));
 
-await client.connect();
+export async function connect(): Promise<void> {
+  if (!client.isOpen) {
+    await client.connect();
+  }
+}
+
+export { client };
