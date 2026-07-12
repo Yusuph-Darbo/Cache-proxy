e2b86f1dc1acd39250ae522e9c2b8cd13648f044
Yusuph Darbo <yusuphdarbo@gmail.com>
2026-07-12 17:37:26 +0100

    get and clear functions

--- src/cache/cache-store.ts
+++ src/cache/cache-store.ts
@@ -10,4 +10,24 @@ export async function connect(): Promise<void> {
   }
 }
 
+export async function get(key: string): Promise<string | null> {
+  const value = await client.get(key);
+  return value;
+}
+
+export async function set(
+  key: string,
+  value: string,
+  ttl: number | null,
+): Promise<void> {
+  if (ttl) {
+    await client.set(key, ttl, { EX: ttl });
+  } else {
+    await client.set(key, value);
+  }
+}
+
+export async function clear(): Promise<void> {
+  await client.flushDb();
+}
 export { client };
