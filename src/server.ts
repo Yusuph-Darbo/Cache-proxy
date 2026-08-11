2a9e726293d08772d90ce3266d0c97cbfd975781
Yusuph Darbo <yusuphdarbo@gmail.com>
2026-08-11 12:50:20 +0100

    imported logic to server

--- src/server.ts
+++ src/server.ts
@@ -1,5 +1,7 @@
 import express from "express";
 import type { Express, Request, Response } from "express";
+import { forwardToOrigin } from "./proxy.js";
+import * as cacheStore from "./cache/cache-store.js";
 
 const app: Express = express();
 
