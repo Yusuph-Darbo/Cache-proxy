1f244f5e3d2498acfb4530e5380e10a2e0c87f49
Yusuph Darbo <yusuphdarbo@gmail.com>
2026-08-11 13:30:57 +0100

    processing argvs

--- src/cli.ts
+++ src/cli.ts
@@ -1,1 +1,36 @@
 #!/usr/bin/env node
+
+import { startServer } from "./server.js";
+import { clear as clearCache } from "./cache/cache-store.js";
+
+interface CliOptions {
+  port?: number;
+  origin?: string;
+  clearCache: boolean;
+}
+
+function parseArgs(argv: string[]): CliOptions {
+  const options: CliOptions = { clearCache: false };
+
+  for (let i = 0; i < argv.length, i++) {
+    const arg = argv[i]
+
+    switch(arg){
+        case '--port':
+            options.port = Number(argv[++i])
+            break
+        
+        case '--origin':
+            options.origin = argv[++i]
+            break
+        
+        case '--clear-cache':
+            options.clearCache = true
+            break
+
+        default:
+            console.error(`Unknown argument: ${arg}`)
+            process.exit(1)
+    }
+  }
+}
